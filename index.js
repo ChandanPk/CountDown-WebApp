37035ab27648c9a5b1d54c0d3ab07eeb92ba4546
ChandanPk <chandanpk9@gmail.com>
2021-05-08 16:24:59 +0530

    Given some functionality for action

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+const timeOut = () => {
+    const destinationTime = new Date("may 10 2021 09:30:00").getTime();
+    let presenTime = new Date().getTime();
+    
+    let difference = destinationTime - presenTime;
+    
+    let day = 1000 * 60 * 60 * 24;
+    let hour = 1000 * 60 * 60;
+    let minute = 1000 * 60;
+    let second = 1000;
+    
+    let dayText = Math.floor(difference / day);
+    let hourText = Math.floor((difference % day) / hour) ;
+    let minuText = Math.floor((difference % hour) / minute);
+    let secondsText = Math.floor((difference % minute) / second);
+    
+    
+        document.querySelector('.days').innerText = dayText;
+        document.querySelector('.hours').innerText = hourText;
+        document.querySelector('.minutes').innerText = minuText;
+        document.querySelector('.seconds').innerText = secondsText;
+        console.log();
+    }
+    setInterval(timeOut, 1000);
